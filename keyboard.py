6e6ad2b63a99ba2fafb3873c94e2db93448c6aa4
Greg "GothAck" Miell <greg@gothack.ninja>
2020-11-18 20:11:55 +0000

    Split plate into lhs and rhs separately printable pieces

--- keyboard.py
+++ keyboard.py
@@ -1,6 +1,7 @@
-from openpyscad import Cube, Cylinder, Difference, Translate, Union
+from openpyscad import Cube, Cylinder, Difference, Translate, Union, Minkowski, Hull, Intersection
 import json
 import sys
+from os import system
 
 SPACING = 18.5
 SIZE = 14
@@ -39,7 +40,7 @@ def plane_key(width, height, depth):
     return k
 
 
-def generate_keys(lines, generate_2=False):
+def generate_keys(lines, generate_2=False, break_at=None, break_at_one_before=False, use_width=True):
     y = 0
     for line in lines:
         x = 0
@@ -48,6 +49,10 @@ def generate_keys(lines, generate_2=False):
         width = 1
         height = 1
         for keydef in line:
+            if break_at is not None and ((x + 1 + (width if use_width else 0)) * SPACING) > break_at:
+                break
+            if break_at is not None and ((x + 2 + (width if use_width else 0)) * SPACING) > break_at and break_at_one_before:
+                break
             if isinstance(keydef, dict):
                 if 'x' in keydef:
                     x += keydef['x']
@@ -97,6 +102,16 @@ def size(lines):
     return max_x + 1, max_y + 1
 
 
+def key_plate_box(lines, depth=1):
+    doc = Translate([0, 0, 0])
+    for x, y, width, height in generate_keys(lines):
+        doc.append(
+            Cube([SPACING * width, SPACING * height, depth], center=True)
+            .translate([SPACING * x, SPACING * y, 0])
+        )
+    return doc
+
+
 def key_plate_clip_cutout(lines, depth=1):
     doc = Translate([0, 0, 0])
     for x, y, width, height in generate_keys(lines):
@@ -119,35 +134,69 @@ def key_plate_cutout(lines, depth=1):
     return doc
 
 
-def upper_cutout(lines, depth=1):
+def upper_cutout(lines, depth=1, break_at=None, break_at_one_before=False, use_width=True):
     doc = Translate([0, 0, 0])
     y = 0
-    for x, y, width, height in generate_keys(lines, generate_2=True):
-        k = Cube([SPACING * width, SPACING * height, depth], center=True)
+    for x, y, width, height in generate_keys(lines, generate_2=True, break_at=break_at, break_at_one_before=break_at_one_before, use_width=not use_width):
+        k = Cube([SPACING * (width if use_width else 1), SPACING * height, depth], center=True)
         doc.append(k.translate([SPACING * x, SPACING * y, 0]))
     return doc
 
 
-def key_plate(lines, padding=5):
-    w, h = size(lines)
-    w *= SPACING
-    h *= SPACING
-    w += padding * 2
-    h += padding * 2
+def key_plate_outer_lhs(lines, padding=5):
+    return Union().append([
+        Hull().append(
+            Minkowski().append([
+                upper_cutout(lines, depth=4.4, break_at=200, break_at_one_before=True, use_width=True),
+                Cube([padding * 2, padding * 2, 0.2]),
+            ])
+        ),
+        Hull().append(
+            Minkowski().append([
+                upper_cutout(lines, depth=4.4, break_at=250, break_at_one_before=True, use_width=False),
+                Cube([padding * 2, padding * 2, 0.2]),
+            ])
+        ),
+        Minkowski().append([
+            upper_cutout(lines, depth=4.4, break_at=250, break_at_one_before=False, use_width=True),
+            Cube([padding, padding, 0.2]),
+        ])
+    ])
+
+
+def key_plate_outer_rhs(lines, padding=5):
+    return Difference().append([
+        Hull().append(
+            Minkowski().append([
+                upper_cutout(lines, depth=4.4),
+                Cube([padding * 2, padding * 2, 0.2]),
+            ])
+        ),
+        Minkowski().append([
+            key_plate_outer_lhs(lines, padding),
+            Cube(0.25, center=True)
+        ])
+    ])
+
+
+def key_plate_lhs(lines, padding=5):
     doc = Difference()
+    doc.append(key_plate_outer_lhs(lines, padding))
     doc.append(
-        Cube([w, h, 4.5], center=True)
-            .translate([-SPACING / 2, -SPACING / 2, 0])
-            .translate([-padding, -padding, 0])
-            .translate([w / 2, h / 2, 0])
-    )
-    doc.append(
-        key_plate_cutout(lines, depth=4.5)
+        key_plate_cutout(lines, depth=5)
     )
     doc.append(
         key_plate_clip_cutout(lines, depth=4.5)
             .translate([0, 0, 1.5])
     )
+    return doc
+
+
+def key_plate_rhs(lines, padding=5):
+    doc = Difference()
+    doc.append(key_plate_outer_rhs(lines, padding))
+    doc.append(key_plate_cutout(lines, depth=5))
+    doc.append(key_plate_clip_cutout(lines, depth=4.5).translate([0, 0, 1.5]))
     return doc
 
 
@@ -168,6 +217,37 @@ def upper_plate(lines, padding=5):
     return doc
 
 
+def upper_plate_2(lines, padding=5):
+    w, h = size(lines)
+    w *= SPACING
+    h *= SPACING
+    w += padding * 2
+    h += padding * 2
+    doc = Difference()
+    doc.append([
+        Translate([0, 0, 0]).append([
+            Hull().append(
+                Minkowski().append([
+                upper_cutout(lines, depth=1.5, break_at=210, break_at_one_before=True),
+                Cube([padding * 2, padding * 2, 0.01], center=True)
+            ])
+            ),
+            # Hull().append(
+                Minkowski().append([
+                upper_cutout(lines, depth=1.5, break_at=210),
+                Cube([padding * 2, padding * 2, 0.01], center=True)
+            ])
+            # ),
+        ]),
+        upper_cutout(lines, depth=1.6, break_at=220),
+    ])
+    return doc
+
+
+def convert(filename_bare):
+    system(f"openscad -o {filename_bare}.stl {filename_bare}.scad")
+
+
 def main(layout_json):
     with open(layout_json) as fh:
         layout = json.load(fh)
@@ -179,20 +259,19 @@ def main(layout_json):
     print(f"Size {size(lines)}")
 
     doc = Translate([0, 0, 0])
-    doc.append(key_plate_clip_cutout(lines))
-    doc.write("key_plate_cutout.scad")
-
-    doc = Translate([0, 0, 0])
-    doc.append(key_plate(lines))
+    doc.append(key_plate_lhs(lines))
+    doc.append(key_plate_rhs(lines, 5).translate([35, 0, 0]))
     doc.write("key_plate.scad")
+    # convert("key_plate")
+    doc2 = Intersection()
+    doc2.append(doc)
+    doc2.append(Translate([200, -20, -10]).append(Cube([100, 180, 50])))
+    doc2.write("key_plate_test.scad")
 
     doc = Translate([0, 0, 0])
-    doc.append(upper_cutout(lines))
-    doc.write("upper_cutout.scad")
-
-    doc = Translate([0, 0, 0])
-    doc.append(upper_plate(lines))
+    doc.append(upper_plate_2(lines))
     doc.write("upper_plate.scad")
+    # convert("upper_plate")
 
 
 if __name__ == "__main__":
